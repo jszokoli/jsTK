732152311d0f00429c18733c04c96ade597c566c
jszokoli <joseph.szokoli@gmail.com>
2018-11-29 23:56:19 -0500

    updated bonus tools

--- jsRenamer/bonus_tools.py
+++ jsRenamer/bonus_tools.py
@@ -6,6 +6,43 @@ class BonusTools(object):
         print 'Initializing jsRenamer BonusTools...'
 
     def cleanUpShapes(self,args=None):
+        sel = cmds.ls(sl=1,type='transform',l=True)
+
+        for xform in sel:
+            # print xform
+            #print xform
+            cmds.delete(xform,ch=1)
+            correctShape = []
+            intermediateShapes = []
+            children = cmds.listRelatives(xform,c=1,f=True)
+            # print children
+            for shapeNode in children:
+                # print shapeNode
+                # print cmds.getAttr(shapeNode+'.intermediateObject')
+                if cmds.getAttr(shapeNode+'.intermediateObject') == True:
+                #if True in cmds.getAttr(shapeNode+'.intermediateObject'):
+                    # print shapeNode
+                    intermediateShapes.append(shapeNode)
+                else:
+                    correctShape.append(shapeNode)
+            # print intermediateShapes
+            # print correctShape 
+            if len(intermediateShapes) > 0:
+                cmds.delete(intermediateShapes)
+            for origShapeName in correctShape:
+                if origShapeName in xform+'Shape':
+                    pass
+                else: 
+                    # print origShapeName
+                    origShapeNameSplit = xform.split('|')[-1]
+                    # print origShapeNameSplit
+                    cmds.rename(origShapeName,origShapeNameSplit+'Shape')
+
+            if len(intermediateShapes) > 0:
+                print xform+' intermediate shapes nodes ('+str(intermediateShapes)+') have been deleted'
+
+'''
+    def cleanUpShapes(self,args=None):
         sel = cmds.ls(sl=1,type='transform')
 
         for xform in sel:
@@ -33,5 +70,10 @@ class BonusTools(object):
 
             if len(intermediateShapes) > 0:
                 print xform+' intermediate shapes nodes ('+str(intermediateShapes)+') have been deleted'
-
+'''
                 
+
+
+
+
+        
